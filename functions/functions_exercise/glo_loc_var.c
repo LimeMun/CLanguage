be29ebb7de43270386706987248df01847162bd6
LimeMun <sungjae.moon@skku.edu>
2024-05-14 15:30:12 +0900

    Fix Code

--- functions/functions_exercise/glo_loc_var.c
+++ functions/functions_exercise/glo_loc_var.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-int globalVar;
+int globalVar = 10;
 void modifyVariables() {
-	int localVar, localVar2;
+	int localVar;
 	globalVar += 100;
 	localVar += 10;
-	localVar2 = localVar2 + 10;
 	printf("Inside function - Global Var: %d, Local Var: %d\n", globalVar, localVar);
 }
 
 int main() {
-	int localVar;
+	int localVar = 20;
 	printf("Before function call - Global Var: %d, Main Local Var: %d\n", globalVar, localVar);
 	modifyVariables();
 	printf("After function call - Global Var: %d, Main Local Var: %d\n", globalVar, localVar);
